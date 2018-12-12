6b4a81e091accd5ca24da6c762ea36abc67dc62e
j-kowal <janokowal@outlook.com>
2018-12-12 18:14:53 +0000

    svg handled

--- src/app.js
+++ src/app.js
@@ -1,2 +1,7 @@
 import 'bulma'
 import './scss/style.scss'
+import * as views from './views'
+
+const app = document.querySelector('#app')
+
+app.innerHTML = views.hero
---
+++ src/views/hero.js
@@ -0,0 +1,13 @@
+import image from '../assets/flame.svg'
+
+export const hero = 
+/*html*/`
+<section class="hero is-fullheight is-link">
+    <div class="hero-body">
+        <div class="container has-text-centered">
+            <p class="title">webpack-start <span class="svg">${image}</span></p>
+            
+        </div>
+    </div>
+</section>
+`
